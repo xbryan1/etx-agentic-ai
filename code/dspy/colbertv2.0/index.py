746a0a776112c300df4b07e1b315edfd5d9a6a58
Mike Hepburn <eformat@gmail.com>
2025-08-24 08:33:40 +1000

    🦩 move dspy 🦩

--- code/dspy/colbertv2.0/index.py
+++
@@ -1,6 +0,0 @@
-from ragatouille import RAGPretrainedModel
-from ragatouille.utils import get_wikipedia_page
-
-RAG = RAGPretrainedModel.from_pretrained("colbert-ir/colbertv2.0")
-my_documents = [get_wikipedia_page("Hayao_Miyazaki"), get_wikipedia_page("Studio_Ghibli")]
-index_path = RAG.index(index_name="my_index", collection=my_documents)
--- code/dspy/colbertv2.0/retrieve2.py
+++
@@ -1,7 +0,0 @@
-from ragatouille import RAGPretrainedModel
-from rich import print
-
-query = "tell me about small models and Neural Magic"
-RAG = RAGPretrainedModel.from_index("/home/mike/git/colbertv2.0/.ragatouille/colbert/indexes/dev-red-rag")
-results = RAG.search(query, k=3)
-print(results)
